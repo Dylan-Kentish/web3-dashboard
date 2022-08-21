daefb20adb8ace0a23581aa1941c6813f1af6adf
Kent <dylankentish@live.co.uk>
2022-08-21 12:58:54 +0100

    Updated to use ReactDOM.createRoot

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { MoralisProvider } from "react-moralis";
 
-ReactDOM.render(
-  <React.StrictMode>
+const r = document.getElementById('root');
+const root = createRoot(r);
+
+root.render(
+  <StrictMode>
     <MoralisProvider
       serverUrl={process.env.REACT_APP_SERVER_URL}
       appId={process.env.REACT_APP_APP_ID}>
-        <App />
+      <App />
     </MoralisProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
